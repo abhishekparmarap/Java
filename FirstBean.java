86f3b34fc20238541dc53046cddb50b57332db90
Abhishek Parmar <abhishek.parmar2020@vitbhopal.ac.in>
2023-07-10 20:12:12 +0530

    javabean file

---
+++ FirstBean.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.awt.*;
+import java.io.Serializable;
+public class FirstBean extends Canvas implements Serializable{
+    public FirstBean (){
+        setSize(60,40);
+        setBackground(Color.red);
+    }
+}
+
