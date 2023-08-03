b839cd42138f3aeceaaccebff17bbb20ddb38bac
Abhishek Parmar <abhishek.parmar2020@vitbhopal.ac.in>
2023-08-03 20:12:20 +0530

    add

---
+++ Emp1.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Emp1 {
+    public static void main(String args[]){
+        Employee e = new Employee();
+        e.setId(101);
+        e.setName("Ram");
+        System.out.println(e.getId());
+        System.out.println(e.getName());
+
+    }
+}
